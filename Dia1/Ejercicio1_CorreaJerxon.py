74b12fa5911c11eb0609f13fe1ade76b5d361f42
JaynAP1 <jerxonmjair@gmail.com>
2024-04-09 11:35:54 -0500

    :D

---
+++ Dia1/Ejercicio1_CorreaJerxon.py
@@ -0,0 +1,10 @@
+print("Holaaaa, bienvenido a la calculadora de sumas")
+
+sum1 = int(input("ingrese el numero 1: "))
+sum2 = int(input("Ingrese el numero 2: "))
+
+
+total= sum1+sum2
+
+print(total)
+
