997746e29a68c7819ac92db2b52c8539999575c2
JaynAP1 <jerxonmjair@gmail.com>
2024-04-12 09:15:20 -0500

    :D

--- Dia3/Ejercicio1_Act_CorreaJerxon.py
+++ Dia3/Ejercicio1_Act_CorreaJerxon.py
@@ -1,22 +1,23 @@
 import secrets
-import string
+import string #Importamos bibliotecas para poder crear las contraseñas seguras
 
 booleano=True
-while booleano==True:
-     
+while booleano==True:#Creamos un While que hara el menu infinitamente, hasta que el usuario decida terminar el programa
+     #Menu de seleccion
     print("""
-    ==============BIENVENIDOS AL HIMALAYA=====================
+    ==============BIENVENIDOS AL HIMALAYA===================== 
         1).Para averiguar que numero es primo.
         2).Para generar una contraseña segura.
         3).Cerrar programa.
     ==========================================================
     """)
 
-    opcion=int(input("Escribe el numero de la opcion a la que deseas ir: "))
+    opcion=int(input("Escribe el numero de la opcion a la que deseas ir: "))#Selecion del menu
 
-    if opcion==1:
+    if opcion==1:#Si se elije uno ira al menu de primos
         booleano2=True
         while booleano2==True:
+            #Menu de primos
             print("""
         ==========================PRIMOS=============================
             En este programa averiguaremos si un numero dado por el
@@ -29,12 +30,12 @@ while booleano==True:
             
             opcion1=int(input("Escribe el numero de la opcion a la que deseas ir: "))
 
-            if opcion1==1:
+            if opcion1==1:#Hara la formulacion de los numeros primos
                     print("===============================================================")
                     NumeroU=int(input("Ingrese el numero que deseas saber si es primo: "))
                     contador = 0
 
-                    for i in range(1,(1+NumeroU)):
+                    for i in range(1,(1+NumeroU)):#formulacion de numeros primos
                         if NumeroU % i == 0:
                             contador=contador+1
 
@@ -42,26 +43,27 @@ while booleano==True:
                         print("Este numero es primo")
                     else:
                         print("Este numero no es primo")
-            elif opcion1==2:
+            elif opcion1==2:#Regresar al menu principal
                  print("Regresaras al menu principal")
                  booleano2=False
-            elif opcion1==3:
+            elif opcion1==3:#Informacion adicional
                  print("=====================INFORMACION===================")
                  print("""Autor:Jerxon Jair Correa Amaris
                           Edad:20
                           Creado:11/04/2024   
                          """)
-    elif opcion==2:
+    elif opcion==2:#Menu de contraseñas
          booleano3 = True
          while booleano3==True:
           
-          LetrasMay = string.ascii_lowercase
-          LetrasMin = string.ascii_uppercase
-          Digitos = string.digits
-          CaracteresEs = string.punctuation
+          LetrasMay = string.ascii_lowercase#Guarda las letras mayusculas en una variable
+          LetrasMin = string.ascii_uppercase#Guarda las letras minusculas en una variable
+          Digitos = string.digits#Guarda los numeros en una variable
+          CaracteresEs = string.punctuation#Guarda los caracteres especiales en una variable
 
-          Alfabeto=LetrasMay+LetrasMin+Digitos+CaracteresEs
+          Alfabeto=LetrasMay+LetrasMin+Digitos+CaracteresEs #Sumamos las variables en una sola para crear un diccionario
 
+#Creamos un menu de las contraseñas con las opciones que tenemos
           print("""
      ======================CONTRASEÑAS================================
           En este programa crearemos una contraseña segura para nuestros usuarios
@@ -70,12 +72,12 @@ while booleano==True:
                     2).Volver al menu principal
                     3).Informacion.
      """)
-          opcion2=int(input("Escribe el numero de la opcion deseada."))
+          opcion2=int(input("Escribe el numero de la opcion deseada."))#Selector de opciones
 
           if opcion2==1:
-               LlevarNu=str(input("Quieres que tu contraseña tenga numeros(SI/NO)"))
-               LLevarAlfa=str(input("Quieres que tu contraseña tenga caracteres especiales (SI/NO)"))
-               MayusONo=str(input("Desea que su contraseña tenga mayusculas(SI/NO)?"))
+               LlevarNu=str(input("Quieres que tu contraseña tenga numeros(SI/NO)"))#Si llevan numeros
+               LLevarAlfa=str(input("Quieres que tu contraseña tenga caracteres especiales (SI/NO)"))#Si lleva caracteres especiales
+               MayusONo=str(input("Desea que su contraseña tenga mayusculas(SI/NO)?"))#Si llevan mayusculas
                if MayusONo=="SI":
                     Alfabeto=LetrasMin+Digitos+CaracteresEs
                     if LlevarNu=="NO":
@@ -83,7 +85,7 @@ while booleano==True:
                          if LLevarAlfa=="NO":
                               Alfabeto=LetrasMin
                     if LlevarNu=="SI":
-                         Alfabeto=LetrasMin+Digitos
+                         Alfabeto=LetrasMin+Digitos #Los condicionales :D
                          if LLevarAlfa=="SI":
                               Alfabeto=LetrasMin+CaracteresEs
                               
@@ -95,22 +97,22 @@ while booleano==True:
                               Alfabeto=LetrasMay
                               
                
-               TamañoCon=int(input("Ingrese la cantidad de caracteres que tendra la contraseña: "))
+               TamañoCon=int(input("Ingrese la cantidad de caracteres que tendra la contraseña: "))#Cantidad de digitos que tiene la contraseña
 
                print("Su contraseña segura se esta generando, presione enter para continuar.")
-               Enter=input("")
+               Enter=input("") #Un esperar tecla muy improvisado
 
                Contraseña=""
-               for i in range(1,(1+TamañoCon)):
+               for i in range(1,(1+TamañoCon)): #Creamos un for que ira creando letra por letra la contraseña
                     Contraseña += "".join(secrets.choice(Alfabeto))
 
-                    if i == TamañoCon:
+                    if i == TamañoCon: #Creamos una condicion que hasta que el ciclo no este completo no imprimira la contraseña, esto para que no genere una escalera
                          print(Contraseña)
 
           elif opcion2==2:
-               booleano3=False
+               booleano3=False #Cambiamos el booleano a falso para terminar con el while y salir al menu principal
 
-          elif opcion2==3:
+          elif opcion2==3:#Informacion adicional
                print("""
 =================================================================
           Una contraseña segura es aquella contraseña que es
@@ -119,7 +121,7 @@ while booleano==True:
           especiales.
 """)
 
-    elif opcion==3:
+    elif opcion==3:#Terminar el programa :D
          print("Saliendo del programa, presione enter para continuar")
          Enter=(input(""))
          booleano=False
