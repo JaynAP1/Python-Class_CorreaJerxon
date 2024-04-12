30d012b012b29d9cfff9372ef7a6563fa0f48019
JaynAP1 <jerxonmjair@gmail.com>
2024-04-12 11:56:58 -0500

    :D

--- Dia4/Ejercicio1_CorreaJerxon.py
+++ Dia4/Ejercicio1_CorreaJerxon.py
@@ -5,7 +5,7 @@ Monedas = [10,5,1] #Creamos una lista que contenga el valor de las monedas
 Booleanito=True #Asignamos un True a una variable que usaremos para el mientras
 while Booleanito==True:#Creamos un mientras que nos repita el ciclo para las veces que queramos hacer el programa tengamos la posibilidad
     UserMoney=int(input("Escribe la cantidad de dinero que tienes: "))#La variable UserMoney es donde se va a guardar el dinero que tiene el ciente y que necesita desglosar
-    if UserMoney > 0:#Creamos un condicional que se encargara de decirle al usuario que debe ingreesar un numero mayor a 0 para hacer el desglose de monedas
+    if UserMoney > 0 and UserMoney < 1001:#Creamos un condicional que se encargara de decirle al usuario que debe ingreesar un numero mayor a 0 para hacer el desglose de monedas
         for i in Monedas:#Creamos un bucle que se va a encargar junto al condicional de revisar el valor la lista uno por uno junto a la cantidad que el usuario ingrese
             if UserMoney >= i:#condicional que comparara el valor del cliente junto a las monedas
                 Plata = UserMoney // i #Creamos una division enteros para que en el resultado no nos de por ejemplo de 25/10=2.5 pero con el // nos dara 2
@@ -30,7 +30,9 @@ while Booleanito==True:#Creamos un mientras que nos repita el ciclo para las vec
                 
 
     elif UserMoney <=0:
-             print("Ingrese un numero valido, que sea mayor a 0")#Debes ingresar un numero valido
+        print("Ingrese un numero valido, que sea mayor a 0")#Debes ingresar un numero valido
+    elif UserMoney >1000:
+        print("Ingrese un numero valido, que sea menor de 1001")#Debes ingresar un numero valido
 
 
 
