e95150a7453b2ab8d905184dc912290e2959228a
JaynAP1 <jerxonmjair@gmail.com>
2024-04-16 16:37:38 -0500

    :D

--- TrabajoQuizz/Sumas_CorreaJerxon.py
+++ TrabajoQuizz/Sumas_CorreaJerxon.py
@@ -1,6 +1,9 @@
 num = [3,2,4]
 
-for i in range (0,len(num),1):
-    if num[i]+num[i+1]==6:
-        print("[",i,",",i+1,"]")
-        break
+if len(num) >= 2 and len(num) <= 10000:
+    for i in range (0,len(num),1):
+        if num[i]+num[i+1]==6:
+            print("[",i,",",i+1,"]")
+            break
+else:
+    print("La lista debe tener mas de un numero")
