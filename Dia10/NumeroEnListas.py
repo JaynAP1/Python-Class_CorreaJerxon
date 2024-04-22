79cb08bcfc04c53d4b0d370644d1a08a68f981f9
JaynAP1 <jerxonmjair@gmail.com>
2024-04-22 12:37:53 -0500

    :D

--- Dia10/NumeroEnListas.py
+++ Dia10/NumeroEnListas.py
@@ -7,8 +7,9 @@ Con=0 #Contador inicia en 0
 Numero=int(input("")) #Numero que va a buscar
 Lista.append(Numero) #Agrega el numero
 Lista.sort() #Ordenar la lista
+NewList=list(set(Lista))
 #Ciclo
-for i in Lista: #Ciclamos la lista
+for i in NewList: #Ciclamos la lista
     Con+=1 #Contador que va a llevar la posicion
     if Numero == i: #Si el numero se encuentra imprimira la posicion
         print(Con-1)
